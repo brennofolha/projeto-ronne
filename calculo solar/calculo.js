821b3fbce8db555c13dc4494b7d44c0f55d7d693
Brenno M. Folha <folhamommbrenno@gmail.com>
2023-06-10 21:36:56 -0400

    fiz algumas alterações

--- calculo solar/calculo.js
+++ calculo solar/calculo.js
@@ -53,40 +53,41 @@ function calcular() {
   var resultadoElement = document.getElementById("resultado");
   resultadoElement.textContent = `Olá, ${nome}! O valor do projeto vai ficar entre: R$${formatarValor(resultado1)} e R$${formatarValor(resultado2)} já instalado (produto + instalação + projeto + homologação + mão de obra)`;
 }
+function enviarMensagemWhatsApp(resultado) {
+  // Substitua as credenciais do Nexmo pelas suas próprias
+  const apiKey = '22b94111';
+  const apiSecret = '7ZTnIxu7TWyXPlmF';
+  const vonage = new Vonage({ apiKey, apiSecret });
 
+  const mensagem = `Olá, ${nome}! O valor do projeto vai ficar entre: R$${formatarValor(resultado1)} e R$${formatarValor(resultado2)} já instalado (produto + instalação + projeto + homologação + mão de obra)`;
 
-async function enviarMensagemWhatsApp(mensagem) {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://web.whatsapp.com');
+  // Substitua os números de telefone pelo remetente e destinatário reais
+  const remetente = '+5569992474003';
+  const destinatario = '+556993817243';
 
-  // Aguarde o usuário fazer o login manualmente no WhatsApp Web
-
-  // Localize o campo de pesquisa e digite o número de telefone ou nome do contato
-  const campoPesquisa = await page.$('div[role="combobox"]');
-  await campoPesquisa.type('+556993817243');
-
-  // Aguarde a lista de resultados de pesquisa aparecer
-  await page.waitForSelector('div[role="option"]');
-
-  // Selecione o primeiro resultado da pesquisa
-  await page.keyboard.press('ArrowDown');
-  await page.keyboard.press('Enter');
-
-  // Espere o campo de entrada de mensagem aparecer
-  await page.waitForSelector('div[contenteditable="true"][data-tab="6"]');
-
-  // Digite a mensagem
-  await page.type('div[contenteditable="true"][data-tab="6"]', mensagem);
-
-  // Envie a mensagem pressionando Enter
-  await page.keyboard.press('Enter');
-
-  // Aguarde um curto período de tempo antes de fechar o navegador
-  await page.waitForTimeout(2000);
-
-  await browser.close();
+  vonage.channel.send(
+    { "type": "whatsapp", "number": remetente },
+    { "type": "whatsapp", "number": destinatario },
+    {
+      "content": {
+        "type": "text",
+        "text": mensagem
+      }
+    },
+    (err, data) => {
+      if (err) {
+        console.error('Erro ao enviar mensagem:', err);
+      } else {
+        console.log('Mensagem enviada com sucesso:', data.message_uuid);
+      }
+    }
+  );
 }
+
+
+  enviarMensagemWhatsApp(resultado1);
+
+
 
 
 
